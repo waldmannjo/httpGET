61b5dd04077a10efc4baaf0f3db737d972a2cf0d
Joachim Waldmann <waldmann.jo@gmail.com>
2019-05-09 11:25:58 +0200

    improve error handling

--- httpGETrequest.js
+++ httpGETrequest.js
@@ -1,31 +1,32 @@
 const http = require('http');
 const colors = require('colors');
-
-function printResult(movie) {
-  console.log(movie);
-}
-
-
-function getMovie(movie) {
-  const url = `http://www.omdbapi.com/?apikey=${api.key}&t=${movie}`;
-  const request = http.get(url, response => {
-    if(response.statusCode === 200) {
-      let body = '';
-      response.on('data', data => {
-        body += data.toString();
-      }); // end on data
-      response.on('end', () => {
-        const dataParsed = JSON.parse(body);
-        console.log(dataParsed);
-      })
-    } // end if
-  });
-}
-
-const query = process.argv.slice(2);
 try {
-  const api = require('./apikey.json');
+  const api = require('./api_key/apikey.json');
+
+  function getMovie(movie) {
+
+    const url = `http://www.omdbapi.com/?apikey=${api.key}&t=${movie}`;
+    const request = http.get(url, response => {
+      if(response.statusCode === 200) {
+        let body = '';
+        response.on('data', data => {
+          body += data.toString();
+        }); // end on data
+        response.on('end', () => {
+          const dataParsed = JSON.parse(body);
+          console.log(dataParsed);
+        })
+      } // end if
+    });
+  }
+
+  function printResult(movie) {
+    console.log(movie);
+  }
+
+  const query = process.argv.slice(2);
   getMovie(query.join('+'));
-} catch(e) {
+
+} catch (e) {
   console.error(e.message.red);
 }
